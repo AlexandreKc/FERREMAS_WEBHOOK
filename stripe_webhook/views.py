4ff918492b1324c360f70465a3acf0b147cad27e
Sebastian <sebastiancarrera2021@gmail.com>
2025-05-14 16:03:01 -0400

    jsonresponse

--- stripe_webhook/views.py
+++ stripe_webhook/views.py
@@ -4,6 +4,7 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 import stripe
 import json
+from django.http import JsonResponse
 from django.conf import settings
 stripe.api_key = 'sk_test_51ROTKbC0ISZZKwGbD573Oh5wcePqMB0VCyCo73LJhb2pS5kJ3c1iGB0j7bNum2RYUCTWSBOFiujiFXmzzBGKj8Jk00Dgk6Ue1k'
 WEBHOOK_SECRET = 'whsec_mslWQRzRFrhNnFPh355UjaqqNqHSkPgI'
